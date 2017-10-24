dd6de6bc9e4b1281e4c5f44ae66a56af8aa1a43b
psli0101 <p.s.li19980101@gmail.com>
2017-10-24 11:49:51 +0800

    calculate bmi

--- app/src/main/java/com/psli/bmi/MainActivity.java
+++ app/src/main/java/com/psli/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.psli.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bmi(View view) {
+        //System.out.println("Cat");
+        Log.d("MainActivity", "testing bmi method");
+        EditText weight = (EditText) findViewById(R.id.ed_weight);
+        EditText height = (EditText) findViewById(R.id.ed_height);
+        float w = Float.parseFloat(weight.getText().toString());
+        float h = Float.parseFloat(height.getText().toString());
+        float bmi = w/(h*h);
+        Log.d("BMI", String.valueOf(bmi));
     }
 }
